935ab085bd7d1c7080ec411b4ef7b5d7d95217b4
Okocha <oko23015@byui.edu>
2024-05-06 02:23:00 -0700

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       
+        List<int> myList = new List<int>();
+
+        Console.WriteLine("Enter number : ");
+        int myNumbers = int.Parse(Console.ReadLine());
+
+        myList.Add(myNumbers);
+
+        while(myNumbers != 0)
+        {
+            Console.WriteLine("Enter number : ");
+            myNumbers = int.Parse(Console.ReadLine());
+
+            myList.Add(myNumbers);            
+        }
+
+        
+        foreach(int item in myList)
+            {
+                Console.WriteLine(item);
+
+                
+            }
+
+        int itemNumbers = myList.Count;
+
+        Console.WriteLine(itemNumbers);
+
+        int appearance = itemNumbers - 1;
+
+        Console.WriteLine(appearance);
+
+        int sum = 0;
+
+
+        for(int x = 0; x < itemNumbers; x++)
+        {
+            
+            Console.WriteLine(x);
+
+            sum += myList[x];
+            Console.WriteLine($"The sum is : {sum}");
+            
+            
+        }
+        
+
+        int myAverage = sum / appearance;
+
+        Console.WriteLine($"The average is : {myAverage}");
+
+        int max = myList[0];
+
+        foreach(int special in myList)
+        {
+            if (special > max)
+            {
+                max = special;
+            }
+            Console.WriteLine($"The maximum number is : {max}");
+
+        
+        }
     }
 }
